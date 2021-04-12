5a593bc6514cd86af3cfeef28a9061df6cf6c9c2
bandarProgramer <bandar.programer@gmail.com>
2021-04-12 10:44:18 +0300

    finish

--- src/main/java/bandar/employee/Controller/EmployeeController.java
+++ src/main/java/bandar/employee/Controller/EmployeeController.java
@@ -3,12 +3,20 @@ package bandar.employee.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import bandar.employee.Entity.Employee;
+import bandar.employee.Exception.ResourceNotFoundException;
 import bandar.employee.Repository.EmployeeRepository;
 
 @RestController
@@ -21,5 +29,27 @@ public class EmployeeController {
 	@GetMapping("employee")
 	public List<Employee> getEmployee(){
 		return employeeRepo.findAll();
+	}
+	
+	@PostMapping("employee")
+	public Employee save(@RequestBody Employee employee) {
+		return employeeRepo.save(employee);
+	}
+	
+	@GetMapping("employee/{id}")
+	public Employee getEmployeeById(@PathVariable Long id) {
+		return employeeRepo.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+	}
+	
+	@PutMapping("employee")
+	public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee){
+		return new ResponseEntity<Employee>(employeeRepo.save(employee),HttpStatus.OK);
+	}
+	
+	@DeleteMapping("employee/{id}")
+	public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable Long id){
+		employeeRepo.deleteById(id);
+		return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
 	}
 }
--- src/main/java/bandar/employee/Entity/Employee.java
+++ src/main/java/bandar/employee/Entity/Employee.java
@@ -1,6 +1,5 @@
 package bandar.employee.Entity;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
